8cca10ecd0aafc1476538b25076ae8dc2efb8fde
Harsh Doshi <harsh@brightroll.com>
2013-02-01 02:05:24 -0800

    refactor a little

--- online-judge/blocks.cc
+++ online-judge/blocks.cc
@@ -44,8 +44,6 @@ struct node * move_blocks_till(int a, int pos_a) {
 void move_over(int a, int b) {
   int pos_a = block_position[a];
   int pos_b = block_position[b];
-
-  if (pos_a == pos_b) return;
   list top_a = move_blocks_till(a, pos_a);
   blocks[pos_a] = top_a->next;
   insert_block(top_a, pos_b);
@@ -54,9 +52,6 @@ void move_over(int a, int b) {
 void move_onto(int a, int b) {
   int pos_a = block_position[a];
   int pos_b = block_position[b];
-
-  if (pos_a == pos_b) return;
-
   list top_a = move_blocks_till(a, pos_a);
   blocks[pos_a] = top_a->next;
   move_blocks_till(b, pos_b);
@@ -66,9 +61,6 @@ void move_onto(int a, int b) {
 void pile_over(int a, int b) {
   int pos_a = block_position[a];
   int pos_b = block_position[b];
-
-  if (pos_a == pos_b) return;
-
   list head = blocks[pos_a];
   list tail = head;
   while (tail->block_num != a) {
@@ -85,21 +77,19 @@ void pile_over(int a, int b) {
 }
 
 void pile_onto(int a, int b) {
-  int pos_a = block_position[a];
   int pos_b = block_position[b];
-
-  if (pos_a == pos_b) return;
-
   move_blocks_till(b, pos_b);
   pile_over(a, b);
 }
 
 void print() {
+
   for (int i = 0; i < nb; i++ ) {
     list b = blocks[i];
     cout << i <<":";
 
     std::vector<int> v;
+    v.reserve(nb);
     while (b)  {
       v.push_back(b->block_num);
       b = b->next;
@@ -128,28 +118,24 @@ int main() {
     cin >> a;
     cin >> what;
     cin >> b;
-    if ( (a > (nb - 1)) || (b > (nb - 1)) ) {
-      continue;
-    }
-    if (a < 0 || b < 0) {
-      continue;
-    }
-    if (action == "move") {
-      if (what == "onto") {
-        move_onto(a,b);
+    if (a != b && block_position[a] != block_position[b]) {
+      if (action == "move") {
+        if (what == "onto") {
+          move_onto(a,b);
+        }
+        else if (what == "over") {
+          move_over(a,b);
+        }
       }
-      else if (what == "over") {
-        move_over(a,b);
-      }
-    }
-    else if (action == "pile") {
-      if (what == "onto") {
-        pile_onto(a,b);
-      }
-      else if (what == "over") {
-        pile_over(a,b);
-      }
+      else if (action == "pile") {
+        if (what == "onto") {
+          pile_onto(a,b);
+        }
+        else if (what == "over") {
+          pile_over(a,b);
+        }
 
+      }
     }
     cin >> action;
   }
