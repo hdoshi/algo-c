d1ba18808105e28ff299ed4235c60a220d4bd4de
Harsh Doshi <harsh@brightroll.com>
2013-02-13 22:27:23 -0800

    finally accepted. Turned out to be format issues

--- online-judge/currency.cc
+++ online-judge/currency.cc
@@ -10,7 +10,6 @@
 
 using namespace std;
 
-
 struct res {
   vector<int> path;
   double cost;
@@ -20,37 +19,27 @@ struct res {
 // paths[i][j][s].cost = max(paths[i][k][1].cost * paths[k][j][s-1].cost, paths[i][j][s].cost)
 
 int main() {
-  int previous = 0;
   int n;
+
   while (cin >> n) {
-
-    if (previous)
-      cout << endl;
-
-    previous = 1;
-
-    double w[n][n];
     struct res paths[n + 1][n][n];
+    double w[n][n];
     for (int i = 0; i < n; i++) {
-      w[i][i] = 1;
-    }
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if(i == j) continue;
-        cin >> w[i][j];
-      }
+      w[i][i] = 1.0;
     }
 
     memset(paths, 0, sizeof(paths));
-
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
+        if(i != j)
+          cin >> w[i][j];
         paths[0][i][j].cost = 0;
         paths[1][i][j].cost = w[i][j];
         paths[1][i][j].path.push_back(i);
         paths[1][i][j].path.push_back(j);
       }
     }
+
 
     for (int steps = 2; steps < n + 1; steps++) {
       for (int k = 0; k < n; k++) {
@@ -60,14 +49,15 @@ int main() {
               paths[steps][i][j].cost = paths[1][i][k].cost * paths[steps - 1][k][j].cost;
               paths[steps][i][j].path.clear();
               paths[steps][i][j].path.push_back(i);
-              //add full path k-j
               paths[steps][i][j].path.insert(paths[steps][i][j].path.end(),paths[steps - 1][k][j].path.begin() ,paths[steps - 1][k][j].path.end());
             }
+            if (paths[steps][i][i].cost > 1.01 || paths[steps][j][j].cost > 1.01 || paths[steps][k][k].cost > 1.01)
+              goto end;
           }
         }
       }
     }
-
+    end:
     int chosen_one = -1;
     int minl = 22;
     for (int i = 0; i < n; i++ ) {
@@ -77,18 +67,25 @@ int main() {
             chosen_one = i;
             minl = steps;
           }
+          break;
         }
       }
     }
 
     if (chosen_one != -1) {
+      int first = 1;
       for(std::vector<int>::iterator it = paths[minl][chosen_one][chosen_one].path.begin(); it != paths[minl][chosen_one][chosen_one].path.end(); ++it) {
-        std::cout << (*it) + 1 << " ";
+        if (!first) {
+          cout << " ";
+        }
+        cout << (*it) + 1;
+        first = 0;
       }
     }
     else {
       cout << "no arbitrage sequence exists" ;
     }
+    cout << endl;
   }
   return 0;
 }
